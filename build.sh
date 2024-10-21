42b71d8efb6defa59e92c2970678d880199f5e67
Mohammed-Adel56 <168343516+Mohammed-Adel56@users.noreply.github.com>
2024-10-21 21:22:11 +0300

    update vercel

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@ python -m venv venv
 source venv/Scripts/activate
 python -m pip install --upgrade pip
 python -m pip install --upgrade setuptools
-pip install -r requirements.txt
+pip install -r requirements.txt --no-deps
